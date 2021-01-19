4e9f2de760b3c07664ed0ef430c02890017c9c3c
IsraelGonzalez <israelgonzalez235.ig@gmail.com>
2021-01-18 18:05:16 -0800

    brick breaker

---
+++ components.js
@@ -0,0 +1,11 @@
+const BG_IMG = new Image();
+BG_IMG.src = "img/back2c.png";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "img/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "img/life16.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/c4.png";
